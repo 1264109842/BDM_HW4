bd4f4a83636185bbec57d4910997ba74f51a0253
1264109842 <40214255+1264109842@users.noreply.github.com>
2021-12-01 14:52:59 -0500

    Add files via upload

--- copy_of_bdm_hw4_you.py
+++ copy_of_bdm_hw4_you.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1jGwVKQpU5SmQxANK3m2FUzgrcPdX99bV
 """
 
-!pip install pyspark
+#!pip install pyspark
 
 import pyspark
 import json
@@ -46,6 +46,11 @@ def high(x,y):
 def median(values_list):
   med = int(np.median(values_list))
   return med
+  
+udfExpand = F.udf(mapday, MapType(DateType(), IntegerType()))
+udfMedian = F.udf(median, IntegerType())
+udfLow    = F.udf(low, IntegerType())
+udfHigh   = F.udf(high, IntegerType())
 
 if __name__=='__main__':
 
@@ -53,37 +58,29 @@ if __name__=='__main__':
          ['722511'],['722513'],['446110','446191'],['311811','722515'],
          ['445210','445220','445230','445291','445292','445299'],['445110']]
 
-  files = ["test/big_box_grocers", "test/convenience_stores", "test/drinking_places", 
+  files = ["test/big_box_grocers", "test/convenience_stores", "test/drinking_places",
           "test/full_service_restaurants", "test/limited_service_restaurants", "test/pharmacies_and_drug_stores",
           "test/snack_and_bakeries", "test/specialty_food_stores", "test/supermarkets_except_convenience_stores"]
 
-  udfExpand = F.udf(mapday, MapType(DateType(), IntegerType()))
-  udfMedian = F.udf(median, IntegerType())
-  udfLow    = F.udf(low, IntegerType())
-  udfHigh   = F.udf(high, IntegerType())
-
-  newdf = spark.read.csv('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*', header=True)
-  new = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header= True)
+  newdf = spark.read.csv('weekly_pattern', header=True)
+  new = spark.read.csv('core-places-nyc.csv', header= True)
 
   for i in range(len(NAICS)):
-    df = new.where(F.col('naics_code').isin(NAICS[i]))
+      df = new.where(F.col('naics_code').isin(NAICS[i]))
 
-    newDF = newdf.join(df, (newdf.placekey == df.placekey) & (newdf.safegraph_place_id == df.safegraph_place_id), "inner")\
-                .select('date_range_start','visits_by_day')\
-                .withColumn('date', substring('date_range_start',1,10))\
-                .drop('date_range_start')\
-                .select(F.explode(udfExpand('date', 'visits_by_day')).alias('date', 'visits'))
+      newDF = newdf.join(df, (newdf.placekey == df.placekey) & (newdf.safegraph_place_id == df.safegraph_place_id), "inner")\
+                  .select('date_range_start','visits_by_day')\
+                  .withColumn('date', substring('date_range_start',1,10))\
+                  .drop('date_range_start')\
+                  .select(F.explode(udfExpand('date', 'visits_by_day')).alias('date', 'visits'))
 
-    newDFF = newDF.filter((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
-                  .groupBy('date')\
-                  .agg(F.collect_list('visits').alias('visits'))\
-                  .withColumn('year', substring('date',1,4))\
-                  .withColumn('date', regexp_replace('date', '2019', '2020'))\
-                  .withColumn('median', udfMedian('visits').alias('median'))\
-                  .withColumn('low', udfLow('median', 'visits').alias('low'))\
-                  .withColumn('high', udfHigh('median','visits').alias('high'))\
-                  .select('year', 'date', 'median', 'low', 'high')
-
-    newDFF.write.format("csv")\
-          .option("header","true")\
-          .save(files[i])
+      newDFF = newDF.filter((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
+                    .groupBy('date')\
+                    .agg(F.collect_list('visits').alias('visits'))\
+                    .withColumn('year', substring('date',1,4))\
+                    .withColumn('date', regexp_replace('date', '2019', '2020'))\
+                    .withColumn('median', udfMedian('visits').alias('median'))\
+                    .withColumn('low', udfLow('median', 'visits').alias('low'))\
+                    .withColumn('high', udfHigh('median','visits').alias('high'))\
+                    .select('year', 'date', 'median', 'low', 'high')\
+                    .write.option("header", "true").csv(files[i])
