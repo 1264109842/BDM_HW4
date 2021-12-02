963970d2fae07c4571f79c8a590fd87f8488fc49
1264109842 <40214255+1264109842@users.noreply.github.com>
2021-12-01 19:08:10 -0500

    Add files via upload

--- copy_of_bdm_hw4_you.py
+++ copy_of_bdm_hw4_you.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 from pyspark.sql import SparkSession
 import numpy as np
 from pyspark.sql import functions as F
-from pyspark.sql.types import DateType, IntegerType, MapType, StringType, FloatType
-from pyspark.sql.functions import split, col, substring, regexp_replace, explode,broadcast
+from pyspark.sql.types import DateType, IntegerType, MapType, StringType, FloatType, ArrayType
+from pyspark.sql.functions import split, col, substring, regexp_replace, explode,broadcast, stddev
 
 sc = pyspark.SparkContext()
 spark = SparkSession(sc)
@@ -35,22 +35,22 @@ def mapday(s, v):
 
   return result
 
-def low(x, y):
-  diff = int(round(x-np.std(y)))
-  return 0 if diff < 0 else diff
-
-def high(x,y):
-  diff = int(round(x+np.std(y)))
-  return 0 if diff < 0 else diff
-
 def median(values_list):
-  med = int(np.median(values_list))
-  return med
+  med = np.median(values_list)
+  stdev = np.std(values_list)
+  low = int(med-stdev)
+  high = int(med+stdev)
+
+  result = []
+
+  result.append(int(med))
+  result.append(low) if low > 0 else result.append(0)
+  result.append(high) if high > 0 else result.append(0)
+
+  return result 
 
 udfExpand = F.udf(mapday, MapType(DateType(), IntegerType()))
-udfMedian = F.udf(median, IntegerType())
-udfLow    = F.udf(low, IntegerType())
-udfHigh   = F.udf(high, IntegerType())
+udfMedian = F.udf(median, ArrayType(IntegerType()))
   
 if __name__=='__main__':
 
@@ -62,30 +62,21 @@ if __name__=='__main__':
           "/full_service_restaurants", "/limited_service_restaurants", "/pharmacies_and_drug_stores",
           "/snack_and_bakeries", "/specialty_food_stores", "/supermarkets_except_convenience_stores"]
 
-  newdf = spark.read.csv('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*', header=True).cache()
-  new = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header= True).cache()
-
-  data = []
+  newdf = spark.read.csv('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*', header=True)
+  new = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header= True)
 
   for i in range(len(NAICS)):
-      df = new.where(F.col('naics_code').isin(NAICS[i])).cache()
+    df = new.where(F.col('naics_code').isin(NAICS[i]))
 
-      newDF = newdf.join(broadcast(df), (newdf.placekey == df.placekey) & (newdf.safegraph_place_id == df.safegraph_place_id))\
-                   .select(F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))\
-                   .cache()
+    newDF = newdf.join(broadcast(df), (newdf.placekey == df.placekey) & (newdf.safegraph_place_id == df.safegraph_place_id))\
+                  .select(F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))
 
-      newDFF = newDF.filter((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
-                    .groupBy('date')\
-                    .agg(F.collect_list('visits').alias('visits'))\
-                    .withColumn('year', substring('date',1,4))\
-                    .withColumn('date', regexp_replace('date', '2019', '2020'))\
-                    .withColumn('median', udfMedian('visits').alias('median'))\
-                    .withColumn('low', udfLow('median', 'visits').alias('low'))\
-                    .withColumn('high', udfHigh('median','visits').alias('high'))\
-                    .select('year', 'date', 'median', 'low', 'high')\
+    newDFF = newDF.where((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
+                  .groupBy('date')\
+                  .agg(F.collect_list('visits').alias('visits'))\
+                  .withColumn('median', udfMedian('visits'))
+
+    newDFFF = newDFF.select(substring('date',1,4).alias('year'), regexp_replace('date', '2019', '2020').alias('date'), newDFF.median[0].alias('median'),newDFF.median[1].alias('low'),newDFF.median[2].alias('high'))\
                     .repartition(1)\
                     .write.option("header","true")\
                     .csv('test'+files[i])
-      
-      df.unpersist()
-      newDF.unpersist()
