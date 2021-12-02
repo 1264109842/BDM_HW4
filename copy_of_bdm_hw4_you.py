d89fb58fec8f2d55a38fe39671f56b392f1cadd6
1264109842 <40214255+1264109842@users.noreply.github.com>
2021-12-02 17:01:41 -0500

    Add files via upload

--- copy_of_bdm_hw4_you.py
+++ copy_of_bdm_hw4_you.py
@@ -20,9 +20,9 @@ from pyspark.sql import functions as F
 from pyspark.sql.types import DateType, IntegerType, MapType, StringType, ArrayType
 from pyspark.sql.functions import split, col, substring, regexp_replace, explode, broadcast
 
-sc = pyspark.SparkContext()
-spark = SparkSession(sc)
-# spark=SparkSession.builder.appName("pysparkdf").getOrCreate()
+# sc = pyspark.SparkContext()
+# spark = SparkSession(sc)
+spark=SparkSession.builder.appName("pysparkdf").getOrCreate()
 
 def mapday(s, v):
   date_1 = datetime.strptime(s[:10], '%Y-%m-%d')
@@ -36,22 +36,22 @@ def mapday(s, v):
 
   return result
 
-# def median(values_list):
-#   med = np.median(values_list)
-#   stdev = np.std(values_list)
-#   low = int(med-stdev)
-#   high = int(med+stdev)
+def median(values_list):
+  med = np.median(values_list)
+  stdev = np.std(values_list)
+  low = int(med-stdev)
+  high = int(med+stdev)
 
-#   result = []
+  result = []
 
-#   result.append(int(med))
-#   result.append(low) if low > 0 else result.append(0)
-#   result.append(high) if high > 0 else result.append(0)
+  result.append(int(med))
+  result.append(low) if low > 0 else result.append(0)
+  result.append(high) if high > 0 else result.append(0)
 
-#   return result 
+  return result
 
 udfExpand = F.udf(mapday, MapType(DateType(), IntegerType()))
-# udfMedian = F.udf(median, ArrayType(IntegerType()))
+udfMedian = F.udf(median, ArrayType(IntegerType()))
   
 if __name__=='__main__':
 
@@ -72,31 +72,31 @@ if __name__=='__main__':
     newDF = newdf.join(broadcast(df), (newdf.placekey == df.placekey) & (newdf.safegraph_place_id == df.safegraph_place_id))\
                   .select(F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))
 
-
     newDFF = newDF.where((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
                   .groupBy('date')\
-                  .agg(F.percentile_approx('visits',0.5).alias('median'),F.stddev('visits').alias('stddev'))\
-                  .na.fill(0)
+                  .agg(F.collect_list('visits').alias('visits'))\
+                  .withColumn('median', udfMedian('visits'))\
+                  .withColumn('year', substring('date',1,4))\
+                  .withColumn('date', regexp_replace('date', '2019', '2020'))\
+                  .orderBy('year', 'date')
 
-    newDFFF = newDFF.withColumn('low',  when(newDFF['median'] - newDFF['stddev'] < 0, 0)
-                                        .otherwise(newDFF['median'] - newDFF['stddev']))\
-                    .withColumn('high',  (newDFF.median + newDFF.stddev).cast('int'))\
-                    .withColumn('year', substring('date',1,4))\
-                    .withColumn('date', regexp_replace('date', '2019', '2020'))\
-                    .orderBy('year', 'date')
-
-    newDFFFF = newDFFF.select('year', 'date', 'median', 'low', 'high')\
+    newDFFFF = newDFF.select('year', 'date', newDFF.median[0].alias('median'), newDFF.median[1].alias('low'), newDFF.median[2].alias('high'))\
                     .coalesce(1)\
                     .write.format("csv")\
                     .option("header","true")\
                     .save('test'+files[i])
 
-      # newDFF = newDF.where((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
-      #               .groupBy('date')\
-      #               .agg(F.collect_list('visits').alias('visits'))\
-      #               .withColumn('median', udfMedian('visits'))\
-      #               .withColumn('year', substring('date',1,4))\
-      #               .withColumn('date', regexp_replace('date', '2019', '2020'))\
-      #               .orderBy('year', 'date')
+    # newDFF = newDF.where((newDF.date > '2018-12-31') & (newDF.date < '2021-01-01') & (newDF.visits > 0))\
+    #               .groupBy('date')\
+    #               .agg(F.collect_list('visits').alias('visits'),F.stddev('visits').alias('stddev'))\
+    #               .withColumn('visits',F.sort_array('visits'))\
+    #               .withColumn('median', newDF.visits[])
+    #               .na.fill(0)
 
-newDFFF.show(515, False)
+    # # newDFFF = newDFF.withColumn('low',  F.when(newDFF.median - newDFF.stddev < 0.0, 0).otherwise((newDFF.median - newDFF.stddev).cast('int')))\
+    # #                 .withColumn('high',  (newDFF.median + newDFF.stddev).cast('int'))\
+    # #                 .withColumn('year', substring('date',1,4))\
+    # #                 .withColumn('date', regexp_replace('date', '2019', '2020'))\
+    # #                 .orderBy('year', 'date')
+
+newDFF.show()
